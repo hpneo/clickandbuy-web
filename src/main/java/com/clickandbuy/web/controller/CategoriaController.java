20b8673edd2270aa070f9a008ed8f1730ef40633
HPNeo <hpneo@hotmail.com>
2013-10-03 17:11:57 -0500

    Listar categorías desde base de datos

--- src/main/java/com/clickandbuy/web/controller/CategoriaController.java
+++ src/main/java/com/clickandbuy/web/controller/CategoriaController.java
@@ -4,8 +4,14 @@
  */
 package com.clickandbuy.web.controller;
 
+import clickandbuy.upc.edu.core.business.CategoriaBusiness;
+import clickandbuy.upc.edu.core.entity.Categoria;
 import com.clickandbuy.web.bean.CategoriaBean;
 import com.clickandbuy.web.util.WebUtil;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.*;
 
 /**
@@ -18,12 +24,14 @@ public class CategoriaController {
   
   @ManagedProperty("#{param.id}")
   private int id = 0;
-  private CategoriaBean categoria = new CategoriaBean();
+  private Categoria categoria = new Categoria();
+  private List<Categoria> categorias = new ArrayList<Categoria>();
+  CategoriaBusiness categoriaBusiness = new CategoriaBusiness();
   
   public void insertar() {
     System.out.println("========================");
-    System.out.println(this.categoria.getCodigo());
-    System.out.println(this.categoria.getNombre());
+    System.out.println(this.categoria.getCatCodigo());
+    System.out.println(this.categoria.getCatCodigo());
     System.out.println("========================");
     
     WebUtil.redirect("/categorias");
@@ -32,8 +40,8 @@ public class CategoriaController {
   public void actualizar() {
     if (this.categoria != null) {
       System.out.println("========================");
-      System.out.println(this.categoria.getCodigo());
-      System.out.println(this.categoria.getNombre());
+      System.out.println(this.categoria.getCatCodigo());
+      System.out.println(this.categoria.getCatNombre());
       System.out.println("========================");
       
       WebUtil.redirect("/categorias/" + this.id);
@@ -48,20 +56,30 @@ public class CategoriaController {
     this.id = id;
   }
 
-  public CategoriaBean getCategoria() {
+  public Categoria getCategoria() {
     if (this.id == 0) {
-      this.categoria = new CategoriaBean();
+      this.categoria = new Categoria();
     }
     else {
-      this.categoria = new CategoriaBean(); //CategoriaBusiness.obtener(this.id);
-      this.categoria.setCodigo(this.id);
-      this.categoria.setNombre("Abarrotes");
+      try {
+	this.categoria = this.categoriaBusiness.getCategoriaByCode(this.id);
+      } catch (Exception ex) {
+	this.categoria = new Categoria();
+      }
     }
     
     return this.categoria;
   }
 
-  public void setCategoria(CategoriaBean categoria) {
+  public void setCategoria(Categoria categoria) {
     this.categoria = categoria;
+  }
+  
+  public List<Categoria> getCategorias() {
+    try {
+      return categoriaBusiness.listCategoria();
+    } catch (Exception ex) {
+      return new ArrayList<Categoria>();
+    }
   }
 }
