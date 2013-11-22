e5173d32fd6bf2c4d4bec2bf9b263a66be0e9da4
Julio <jano_26191@hotmail.com>
2013-11-21 19:27:02 -0500

    Refactory

--- src/main/java/com/clickandbuy/web/bean/ProductoBean.java
+++ src/main/java/com/clickandbuy/web/bean/ProductoBean.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.clickandbuy.web.bean;
 
 /**
@@ -77,5 +73,4 @@ public class ProductoBean implements java.io.Serializable {
   public void setPrecioVenta(double precioVenta) {
     this.precioVenta = precioVenta;
   }
-  
 }
